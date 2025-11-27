87875bdaf473f87385fee64ccb55a7ae9e29eb0f
marushana <marushana@yandex.ru>
2025-11-27 18:38:10 +0200

    soft assert

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -5,8 +5,10 @@ import enums.Gender;
 import enums.Hobbies;
 import enums.StateCity;
 import manager.AppManager;
+import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.FormsPage;
 import pages.HomePage;
 import pages.PracticeFormPage;
@@ -14,8 +16,9 @@ import pages.PracticeFormPage;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PracticeFormTests extends AppManager {
 
+public class PracticeFormTests extends AppManager {
+    SoftAssert softAssert = new SoftAssert();
     @Test
     public void studentRegFormPositiveTest(){
         List<Hobbies> hobbies = new ArrayList<>();
@@ -31,8 +34,66 @@ public class PracticeFormTests extends AppManager {
         new HomePage(getDriver()).clickBtnForms();
         new FormsPage(getDriver()).clickBtnPraccticeForm();
         new PracticeFormPage(getDriver()).typePracticeForm(student);
+//        Assert.assertTrue(new PracticeFormPage(getDriver())
+//                .validateMessage("Wrong text"));
+
+        System.out.println("Test pass");
+
         Assert.assertTrue(new PracticeFormPage(getDriver())
                 .validateMessage("Thanks for submitting the form"));
+
+    }
+
+    @Test
+    public void studentRegFormPositiveTestWithSoftAssert(){
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.SPORTS);
+        hobbies.add(Hobbies.READING);
+        Student student = new Student("Marina", "Suovich",
+                "tyure@yandex.ru", Gender.FEMALE,
+                "0456123789", "13 Apr 1986",
+                "Math,Physics,Chemistry",
+                hobbies, "", "address 1",
+                StateCity.UTTAR_PRADESH.getState(),
+                StateCity.UTTAR_PRADESH.getCity()[1]);
+        new HomePage(getDriver()).clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPraccticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
+        softAssert.assertTrue(new PracticeFormPage(getDriver())
+                .validateMessage("Wrong text"), "validate Wrong message");
+
+        System.out.println("Test pass");
+
+        softAssert.assertTrue(new PracticeFormPage(getDriver())
+                .validateMessage("Thanks for submitting the form"), "validate right message");
+        softAssert.assertAll();
+
+    }
+
+    @Test
+    public void studentRegFormPositiveTestWithSoftAssert_CheckFields(){
+        List<Hobbies> hobbies = new ArrayList<>();
+        hobbies.add(Hobbies.SPORTS);
+        hobbies.add(Hobbies.READING);
+        Student student = new Student("Marina", "Suovich",
+                "tyure@yandex.ru", Gender.FEMALE,
+                "0456123789", "13 Apr 1986",
+                "Math,Physics,Chemistry",
+                hobbies, "", "address 1",
+                StateCity.UTTAR_PRADESH.getState(),
+                StateCity.UTTAR_PRADESH.getCity()[1]);
+        new HomePage(getDriver()).clickBtnForms();
+        new FormsPage(getDriver()).clickBtnPraccticeForm();
+        new PracticeFormPage(getDriver()).typePracticeForm(student);
+        softAssert.assertTrue(new PracticeFormPage(getDriver())
+                .validateMessage("Thanks for submitting the form"), "validate right message");
+        softAssert.assertTrue(getDriver().findElement(By.xpath("//tbody/tr/td[2]")).getText()
+                .contains(student.getLastName()),"validate last name");
+        softAssert.assertTrue(getDriver().findElement(By.xpath("//tbody/tr/td[2]")).getText()
+                .contains(student.getName()),"validate name");
+        softAssert.assertEquals(getDriver().findElement(By.xpath("//tbody/tr[2]/td[2]"))
+                .getText(), student.getEmail(), "validate email");
+        softAssert.assertAll();
 
     }
 
