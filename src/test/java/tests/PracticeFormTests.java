e690d231cab5c9c9c03c3c2300d5cdc1fa4ed959
marushana <marushana@yandex.ru>
2025-12-24 20:58:08 +0200

    Git actions

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 public class PracticeFormTests extends AppManager {
     SoftAssert softAssert = new SoftAssert();
-    @Test
+    @Test(groups = "smoke")
     public void studentRegFormPositiveTest(){
         List<Hobbies> hobbies = new ArrayList<>();
         hobbies.add(Hobbies.SPORTS);
